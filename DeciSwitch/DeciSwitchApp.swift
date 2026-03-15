99a242bfcf51df0e67c09401889b2ca5c721da7f
winner14 <winnerkpodo0@gmail.com>
2026-03-15 03:22:47 +0000

    Initial Commit

---
+++ DeciSwitch/DeciSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeciSwitchApp.swift
+//  DeciSwitch
+//
+//  Created by Winner on 15/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct DeciSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
